59cef986f64855a51b2a2da8bdeb334605bb1f85
GANESHARAVIND-124 <ganesharavind124@gmail.com>
2024-06-14 15:17:22 +0530

    v1

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 import os
 from flask import Flask, render_template, request, redirect
-from flask_pymongo import PyMongo
-from flask_admin import Admin, BaseView, expose
+from flask_pymongo import PyMongo  # type: ignore
 
 app = Flask(__name__)
 
 # Set up MongoDB
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI', 'mongodb://mongo:27017/my_flask_app_db')
 mongo = PyMongo(app)
-
-# Define Admin view
-class FeedbackAdminView(BaseView):
-    @expose('/')
-    def index(self):
-        feedbacks = mongo.db.feedback.find()
-        return self.render('admin/index.html', feedbacks=feedbacks)
-
-# Admin interface setup
-admin = Admin(app, name='Feedback Admin', template_mode='bootstrap3')
-admin.add_view(FeedbackAdminView(name='Feedback'))
 
 @app.route('/')
 def index():
